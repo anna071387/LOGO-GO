94cbd4c42938535031997c2122430d36f68fa709
Anna Gallagher <annagallagher713@gmail.com>
2023-08-15 16:15:43 -0600

    updated index.js and shape.js, circle is rendering

--- index.js
+++ index.js
@@ -13,25 +13,46 @@ const writeFileAsync = promisify(writeFile);
 function userPrompt() {
   return prompt(questions);
 }
-async function init() {
+  async function init() {
   try {
     const response = await userPrompt();
-    console.log(JSON.stringify(response, null, "\t"));
+      console.log(response);
 
+  var shape;
     if (response.shapes == "Circle") {
-      shapes = Circle;
-    } else if (response.shapes == "Triangle") {
-      shapes = Triangle;
-    } else {
+         shape= new Circle(response.contents, response.color, response.text)
+    
+    } else if (response.shape == "Triangle") {
+      // myCar = new Car("Ford");
+      shape= new Triangle(response.contents, response.color, response.text);
+    } else 
+       {
+      shape= new Square(response.contents, response.color, response.text);
     }
+  
+
+console.log(shape);
+console.log(shape.render());
+
+
+var logo=`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+     ${shape.render()}
+
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${shape.textcolor}" >${shape.text}</text>
+
+      </svg>`
 
     await writeFileAsync("Logo.svg", logo);
 
+
+      // console.log(logo);
     console.log("Successfully created .svg");
   } catch (err) {
     console.log(err);
-  }
-}
+  }}
+
+
 
 // Function call to initialize app
 init();
